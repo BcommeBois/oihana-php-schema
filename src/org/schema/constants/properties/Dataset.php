ff47beff693c3df2ed475c46f8f81a47abcc959d
Marc Alcaraz <ekameleon@gmail.com>
2025-06-18 11:38:42 +0200

    Update Dataset.php

--- src/org/schema/constants/properties/Dataset.php
+++ src/org/schema/constants/properties/Dataset.php
@@ -4,10 +4,15 @@ namespace org\schema\constants\properties;
 
 trait Dataset
 {
-    const string DISTRIBUTION             = 'distribution' ;
+    const string DISTRIBUTION = 'distribution' ;
+
     const string INCLUDED_IN_DATA_CATALOG = 'includedInDataCatalog' ;
-    const string ISSN                     = 'issn' ;
-    const string MEASUREMENT_METHOD       = 'measurementMethod' ;
-    const string MEASUREMENT_TECHNIQUE    = 'measurementTechnique' ;
-    const string VARIABLE_MEASURED        = 'variableMeasured' ;
+
+    const string ISSN = 'issn' ;
+
+    const string MEASUREMENT_METHOD = 'measurementMethod' ;
+
+    const string MEASUREMENT_TECHNIQUE = 'measurementTechnique' ;
+
+    const string VARIABLE_MEASURED = 'variableMeasured' ;
 }
