994e84521db2c5456e6391873e1350b278b247bf
Marc Alcaraz <ekameleon@gmail.com>
2025-12-04 16:32:37 +0100

    Update GeoCoordinates.php

--- src/org/schema/constants/traits/GeoCoordinates.php
+++ src/org/schema/constants/traits/GeoCoordinates.php
@@ -4,13 +4,17 @@ namespace org\schema\constants\traits;
 
 trait GeoCoordinates
 {
-    const string DISTANCE  = 'distance'  ;
-    const string ELEVATION = 'elevation' ;
-    const string LATITUDE  = 'latitude'  ;
-    const string LONGITUDE = 'longitude' ;
+    // ---- Standards
 
-    const string GEO_DISTANCE  = 'geo.distance'  ;
-    const string GEO_ELEVATION = 'geo.elevation' ;
-    const string GEO_LATITUDE  = 'geo.latitude'  ;
-    const string GEO_LONGITUDE = 'geo.longitude' ;
+    public const string DISTANCE  = 'distance'  ;
+    public const string ELEVATION = 'elevation' ;
+    public const string LATITUDE  = 'latitude'  ;
+    public const string LONGITUDE = 'longitude' ;
+
+    // ---- Dot notation
+
+    public const string DISTANCE_PATH  = 'geo.distance'  ;
+    public const string ELEVATION_PATH = 'geo.elevation' ;
+    public const string LATITUDE_PATH  = 'geo.latitude'  ;
+    public const string LONGITUDE_PATH = 'geo.longitude' ;
 }
