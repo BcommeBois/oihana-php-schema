1d61751eb219841b5afef5b2c0e8f3d464953cd3
Marc Alcaraz <ekameleon@gmail.com>
2026-05-07 16:31:17 +0200

    Allow array for Action::$object property
    Update src/org/schema/Action.php to include array in the union type for the $object property (docblock and typed property). This change allows $object to hold multiple Things/strings (arrays) instead of only a single value.

--- src/org/schema/Action.php
+++ src/org/schema/Action.php
@@ -26,9 +26,9 @@ class Action extends Thing
 
     /**
      * The direct performer or driver of the action (animate or inanimate). E.g. John wrote a book.
-     * @var Person|Organization|string|null
+     * @var Person|Organization|string|array|null
      */
-    public null|Person|Organization|string $agent ;
+    public null|Person|Organization|array|string $agent ;
 
     /**
      * The endTime of something.
@@ -42,9 +42,9 @@ class Action extends Thing
 
     /**
      * For failed actions, more information on the cause of the failure.
-     * @var null|Thing|string
+     * @var null|Thing|array|string
      */
-    public null|Thing|string $error ;
+    public null|Thing|array|string $error ;
 
     /**
      * The object that helped the agent perform the action. E.g. John wrote a book with a pen.
@@ -60,9 +60,9 @@ class Action extends Thing
 
     /**
      * The object upon which the action is carried out, whose state is kept intact or changed. Also known as the semantic roles patient, affected or undergoer (which change their state) or theme (which doesn't). E.g. John read a book.
-     * @var null|Thing|string
+     * @var null|Thing|array|string
      */
-    public null|Thing|string $object ;
+    public null|Thing|array|string $object ;
 
     /**
      * Other co-agents that participated in the action indirectly. E.g. John wrote a book with Steve.
@@ -80,9 +80,9 @@ class Action extends Thing
 
     /**
      * The result produced in the action. E.g. John wrote a book.
-     * @var null|Thing|string
+     * @var null|Thing|array|string
      */
-    public null|Thing|string $result ;
+    public null|Thing|array|string $result ;
 
     /**
      * The startTime of something. For a reserved event or service (e.g. FoodEstablishmentReservation), the time that it is expected to start. For actions that span a period of time, when the action was performed. E.g. John wrote a book from January to December. For media, including audio and video, it's the time offset of the start of a clip within a larger file.
@@ -91,9 +91,9 @@ class Action extends Thing
 
     /**
      * Indicates a target EntryPoint, or url, for an Action.
-     * @var string|EntryPoint|null
+     * @var string|EntryPoint|array|null
      */
-    public null|string|EntryPoint $target ;
+    public null|string|array|EntryPoint $target ;
 }
 
 
